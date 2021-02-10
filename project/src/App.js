f3db219b83273a7171833293553280778e68c0c6
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-02-09 22:41:57 -0500

    finished with functionality

--- project/src/App.js
+++ project/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Card from './Card'
 
 class App extends React.Component {
-  state = {github: {}, followers: []}
+  state = {github: {}, followers: [], search: ''}
   componentDidMount(){
     axios.get('https://api.github.com/users/chasebianchi')
     .then(res=>this.setState({github: res.data}))
@@ -15,10 +15,23 @@ class App extends React.Component {
     .then(res=>this.setState({followers: res.data}))
     .catch(err=>console.log(err))
   }
+  formChange = (e)=>{
+    this.setState({search: e.target.value})
+  }
+  formSubmit = (e)=>{
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.search}`)
+    .then(res=>this.setState({github: res.data, search: ''}))
+    .catch(err=>console.log(err));
+  }
   render(){
   return (
     <div className="App">
       <h1>Who is this Git?</h1>
+      <form onSubmit={this.formSubmit}>
+        <input onChange={this.formChange} placeholder='enter a github username' type='text' name='searchbar' value={this.state.search}/>
+        <button>Find Git</button>
+      </form>
       <Card github={this.state.github} followers={this.state.followers}/>
     </div>
   );
--- project/src/Card.js
+++ project/src/Card.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv=styled.div`
+width: 100%;
+height: auto;
+display: flex;
+flex-flow: column nowrap;
+img{
+    object-fit: contain;
+}
+`
 
 export default function Card(props) {
-console.log('git', props.github)
+// console.log('git', props.github)
     
 return (
-        <div>
+        <StyledDiv>
         <h2>{props.github.login}</h2>
         <p>{props.github.location}</p>
         <img alt='user avatar' src={props.github.avatar_url}></img>
+        <br/>
         <a href={props.github.html_url}>Go to Github page</a>
         
          <div>Followers:
@@ -15,6 +27,6 @@ return (
                 {props.followers.map(person=><li>{person}</li>)}
             </ul>
         </div>
-        </div>
+        </StyledDiv>
     )
 }
